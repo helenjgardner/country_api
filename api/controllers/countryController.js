a51d7e215fd910e7c2d423c1711e28cff46c3ec4
helenjgardner <helen_gardner@hotmail.com>
2020-01-28 10:26:55 +0000

    added exception handling style functionality

--- api/controllers/countryController.js
+++ api/controllers/countryController.js
@@ -2,22 +2,28 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 exports.list_Country = function (req, res) {
-   const dataSourceURL='https://raw.githubusercontent.com/umpirsky/country-list/master/data/en/country.json';
-   fetch(dataSourceURL)
-   .then((response) => {
-           return response.json();
+  const dataSourceURL = 'https://raw.githubusercontent.com/umpirsky/country-list/master/data/en/country.json';
+  fetch(dataSourceURL)
+    .then((response) => {
+      return response.json();
     })
     .then((myJson) => {
-      // returns {"code":"countryname"}
-      let obj={};
-      obj[req.params.code]=myJson[req.params.code];
-      res.json(obj)
 
-      // returns alternative {country:"countryname"}
-      // res.json({ 'country': myJson[req.params.code] })
-        
+      if (myJson[req.params.code]) {
+        // returns {"code":"countryname"}
+        let obj = {};
+        obj[req.params.code] = myJson[req.params.code];
+        res.status(200).json(obj)
+
+        // returns alternative {country:"countryname"}
+        // res.status(200).json({ 'country': myJson[req.params.code] })
+      }
+      else res.status(200).json({ 'result' : 'country name not found for ' + req.params.code })
     }
-    // add a catch
-    // add return status
-  );
+    )
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
+
+
 };
--- api/routes/countryRoutes.js
+++ api/routes/countryRoutes.js
@@ -1,4 +1,3 @@
-'use strict';
 module.exports = function (app) {
   var countryAPI = require('../controllers/countryController');
 
